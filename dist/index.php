e7a8e35290899e1f4b3565e15d53f78f09f2e79f
Vlad Filchagin <gelenbagen@gmail.com>
2020-02-14 08:47:09 +0300

    corrected vers

---
+++ dist/index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="../43-whiteeng/dist/css/main.css">
+	<title>Nectarin</title>
+</head>
+
+<body class="page">
+
+	<div class="uniqueizer11331">
+		<header class="header">
+			<div class="header__bgBlock">
+				<img src="../43-whiteeng/dist/img/bg.png" alt="background color" class="header__bgColor">
+				<img src="../43-whiteeng/dist/img/Shape.png" alt="shape" class="header__bgShape">
+				<img src="../43-whiteeng/dist/img/pinkMaterial.png" alt="" class="header__pink">
+				<img src="../43-whiteeng/dist/img/white_bg.png" alt="white bg" class="header__bgWhite">
+				<img src="../43-whiteeng/dist/img/man.png" alt="bg man" class="header__bgMan">
+			</div>
+			<div class="container header__container">
+				<img src="../43-whiteeng/dist/img/logo.png" alt="logo" class="header__logo">
+				<h1 class="header__title">Elite Housing estate launch</h1>
+				<img src="../43-whiteeng/dist/img/textLogo.png" alt="" class="header__whiteKhamovniki">
+			</div>
+		</header>
+		<section class="question">
+			<img src="../43-whiteeng/dist/img/building.png" alt="building" class="question__bgBuilding">
+			<img src="../43-whiteeng/dist/img/shadowAside1.png" alt="shadow" class="question__bgShadow">
+			<div class="container">
+				<div class="question__content">
+					<p class="question__wordSt1">HOW</p>
+					<p class="question__wordSt2">to launch this</p>
+					<p class="question__wordSt3">project</p>
+					<p class="question__wordSt4">using only</p>
+					<p class="question__wordSt5">contextual</p>
+					<p class="question__wordSt6">and targeted</p>
+					<p class="question__wordSt7">advertising and</p>
+					<p class="question__wordSt8">have KPIs exceed</p>
+					<p class="question__wordSt9">expected results?</p>
+				</div>
+			</div>
+		</section>
+		<section class="tasks">
+			<img src="../43-whiteeng/dist/img/circlePhoto1.png" alt="couple" class="tasks__circlePhoto1">
+			<img src="../43-whiteeng/dist/img/woman.png" alt="woman" class="tasks__woman">
+			<img src="../43-whiteeng/dist/img/shadowAside2.png" alt="shadow effect" class="tasks__shadow">
+			<img src="../43-whiteeng/dist/img/bigBlueBall.png" alt="blue ball" class="tasks__blueBall">
+			<img src="../43-whiteeng/dist/img/circlePhoto2.png" alt="couple" class="tasks__circlePhoto2">
+			<img src="../43-whiteeng/dist/img/circlePhoto3.png" alt="couple" class="tasks__circlePhoto3">
+			<img src="../43-whiteeng/dist/img/redMaterial.png" alt="red material" class="tasks__redMaterial">
+			<div class="container">
+				<h2 class="tasks__title">Client's aim and <br>Target audience</h2>
+				<p class="tasks__subtitle">Meet the flats sales plan.</p>
+				<div class="tasks__trendsetters trendsetters">
+					<h3 class="trendsetters__title">
+						Active trendsetters
+					</h3>
+					<p class="trendsetters__p">
+						Male/ female 25-40 y.o.<br> Have their own business,<br> top managers, heirs to rich<br> families. Travel
+						a lot.
+					</p>
+					<p class="trendsetters__p">
+						Are aware of<br> modern trendencies in design,<br> architecture and have fine taste.
+					</p>
+					<p class="trendsetters__p">
+						Have the reputation of<br> trendsetters, aesthetes, intellectuals<br> that are knowledgeable about<br>
+						modern art and
+						are
+						first to<br> find out modern cultural tendencies.
+					</p>
+				</div>
+				<div class="tasks__decisionBlock decisionBlock">
+					<h3 class="decisionBlock__title">
+						The solution
+					</h3>
+					<p class="decisionBlock__p">
+						In order to complete<br> this task at the launch<br> phase, we used minimal number<br> of ad sources. Main
+						part of the<br>
+						media-mix was the digital promotion.
+					</p>
+					<p class="decisionBlock__p">
+						Initially, <br> <span>30%</span> of the digital media-mix budget was supposed to be spent on image
+						sources.
+					</p>
+				</div>
+			</div>
+		</section>
+		<section class="about">
+			<div class="container about__container">
+				<p class="about__info about__info_1">Given the fact that the number of offline sources in the main media-mix
+					was
+					minimal, it was necessary to use tools and channels that influence demand for the brand. The reason is,
+					offline
+					sources usually help increase brand awareness and as a result they influence conversion received from
+					organic and paid search.</p>
+				<div class="about__wrapperSwiper noselect">
+					<div class="about__swiper-container">
+						<div class="swiper-wrapper about__swiperItemsContainer">
+							<img src="../43-whiteeng/dist/img/slider1_img1.png" alt="img 1" class="swiper-slide">
+							<img src="../43-whiteeng/dist/img/slider1_img2.png" alt="img 2" class="swiper-slide">
+						</div>
+					</div>
+					<div class="about__sliderButtons">
+						<svg class="about__chevron about__chevron_left">
+							<use xlink:href="../43-whiteeng/dist/svgSprite/sprite.svg#chevron"></use>
+						</svg>
+						<svg class="about__chevron about__chevron_right">
+							<use xlink:href="../43-whiteeng/dist/svgSprite/sprite.svg#chevron"></use>
+						</svg>
+					</div>
+				</div>
+				<div class="about__swiper">
+					<div class="about__miniSlides">
+						<div class="about__miniWrapper">
+							<img src="../43-whiteeng/dist/img/slider1_img1.png" alt="img 1" class="about__miniSlide">
+							<img src="../43-whiteeng/dist/img/slider1_img2.png" alt="img 2" class="about__miniSlide">
+						</div>
+					</div>
+					<p class="about__info about__info_2">The number of offline sources in the main media-mix was minimal, it was
+						necessary to use tools and channels that influence demand for the brand. The reason is, offlinesources
+						usually help increase brand awareness and as a resultthey influence conversion received from organic and
+						paid
+						search.</p>
+				</div>
+			</div>
+		</section>
+		<section class="howItWas">
+			<h2 class="howItWas__title">How we did it</h2>
+			<img src="../43-whiteeng/dist/img/behindPhone.png" alt="bg" class="howItWas__bg">
+			<img src="../43-whiteeng/dist/img/phone.png" alt="phone bg" class="howItWas__phoneBg">
+			<!-- <img src="../43-whiteeng/dist/img/cloud.png" alt="cloud" class="howItWas__cloud"> -->
+			<div class="howItWas__wrapperSwiper noselect">
+				<div class="howItWas__swiper-container">
+					<div class="swiper-wrapper howItWas__swiperItemsContainer">
+						<img src="../43-whiteeng/dist/img/slider2_img1.png" alt="img 1" class="swiper-slide">
+						<img src="../43-whiteeng/dist/img/slider2_img2.png" alt="img 2" class="swiper-slide">
+					</div>
+				</div>
+				<div class="howItWas__sliderButtons">
+					<svg class="howItWas__chevron howItWas__chevron_left">
+						<use xlink:href="../43-whiteeng/dist/svgSprite/sprite.svg#chevron"></use>
+					</svg>
+					<svg class="howItWas__chevron howItWas__chevron_right">
+						<use xlink:href="../43-whiteeng/dist/svgSprite/sprite.svg#chevron"></use>
+					</svg>
+				</div>
+			</div>
+		</section>
+		<section class="results">
+			<div class="container results__container">
+				<h2 class="results__title">Our results</h2>
+				<p class="results__subtitle">Qualitative conversion in volume that exceeded planned volume allowed the sales
+					specialists to meet the target parameters on shows/ bookings.</p>
+				<div class="results__indicators">
+					<div class="indicator">
+						<div class="indicator__left">
+							<img src="../43-whiteeng/dist/img/yandexDirect.png" alt="yandex direct" class="indicator__Ydirect">
+							<p class="indicator__direction">Yandex Direct</p>
+						</div>
+						<div class="indicator__right">
+							<p class="indicator__result">282%</p>
+							<p class="indicator__info">Above target</p>
+						</div>
+					</div>
+					<div class="indicator">
+						<div class="indicator__left">
+							<img src="../43-whiteeng/dist/img/googleAdwords.png" alt="google adwords" class="indicator__Gadwords">
+							<p class="indicator__direction">Google Adwords</p>
+						</div>
+						<div class="indicator__right">
+							<p class="indicator__result">267%</p>
+							<p class="indicator__info">Above target</p>
+						</div>
+					</div>
+					<div class="indicator">
+						<div class="indicator__left">
+							<img src="../43-whiteeng/dist/img/facebook_intagram.png" alt="fb inst" class="indicator__fbInst">
+							<p class="indicator__direction">FB/Instagram</p>
+						</div>
+						<div class="indicator__right">
+							<p class="indicator__result">127%</p>
+							<p class="indicator__info">Above target</p>
+						</div>
+					</div>
+					<div class="indicator">
+						<div class="indicator__left">
+							<img src="../43-whiteeng/dist/img/click.png" alt="click" class="indicator__click">
+							<p class="indicator__direction">Referrals and <br> direct visits</p>
+						</div>
+						<div class="indicator__right">
+							<p class="indicator__result">350%</p>
+							<p class="indicator__info">Above target</p>
+						</div>
+					</div>
+					<div class="indicator">
+						<div class="indicator__left">
+							<p class="indicator__direction indicator__direction_total">Total context</p>
+						</div>
+						<div class="indicator__right">
+							<p class="indicator__result">241%</p>
+							<p class="indicator__info">Above target</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="headOfMarketing">
+			<div class="container headOfMarketing__container">
+				<header class="headOfMarketing__header">
+					<svg class="headOfMarketing__quote headOfMarketing__quote_left">
+						<use xlink:href="../43-whiteeng/dist/svgSprite/sprite.svg#quote_right"></use>
+					</svg>
+					<div class="headOfMarketing__titleBlock">
+						<h2 class="headOfMarketing__title">Tatiana Gorodnicheva</h2>
+						<div class="headOfMarketing__subtitle">Senior Account Manager</div>
+					</div>
+					<svg class="headOfMarketing__quote">
+						<use xlink:href="../43-whiteeng/dist/svgSprite/sprite.svg#quote_right"></use>
+					</svg>
+				</header>
+				<img src="../43-whiteeng/dist/img/gorodnichevaProfile.png" alt="photo" class="headOfMarketing__photo">
+				<footer class="headOfMarketing__footer">
+					<p class="headOfMarketing__footerText">Nectarin agency were able to exceed the expected volumes of target
+						messages at the beginning of the campaign. What’s more using mainly contextual ads guarantees the quality
+						of
+						the calls.</p>
+				</footer>
+			</div>
+		</section>
+		<section class="team">
+			<div class="container team__container">
+				<h2 class="team__title">
+					Project team
+				</h2>
+				<div class="team__members">
+					<div class="member">
+						<img src="../43-whiteeng/dist/img/larinaProfile.png" alt="photo" class="member__photo">
+						<p class="member__name">Ekaterina Larina</p>
+						<p class="member__position">Contextual Advertising Manager</p>
+					</div>
+					<div class="member">
+						<img src="../43-whiteeng/dist/img/belyaevaProfile.png" alt="photo" class="member__photo">
+						<p class="member__name">Tatiana Belyaeva</p>
+						<p class="member__position">Contextual Advertising Manager</p>
+					</div>
+					<div class="member">
+						<img src="../43-whiteeng/dist/img/shevchenkoProfile.png" alt="photo" class="member__photo">
+						<p class="member__name">Tatiana Shevchenko</p>
+						<p class="member__position">Head of Context Department</p>
+					</div>
+					<div class="member">
+						<img src="../43-whiteeng/dist/img/gorodnichevaProfile.png" alt="photo" class="member__photo">
+						<p class="member__name">Tatiana Gorodnicheva</p>
+						<p class="member__position">Senior Account Manager</p>
+					</div>
+					<div class="member">
+						<img src="../43-whiteeng/dist/img/gartmanProfile.png" alt="photo" class="member__photo">
+						<p class="member__name">Sofia Gartman</p>
+						<p class="member__position">Account Director</p>
+					</div>
+					<div class="member">
+						<img src="../43-whiteeng/dist/img/bessonovaProfile.png" alt="photo" class="member__photo">
+						<p class="member__name">Anna Bessonova</p>
+						<p class="member__position">Head Media Planner</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="footer">
+			<img src="../43-whiteeng/dist/img/footerLine.png" alt="decoreLines" class="footerBg">
+		</footer>
+	</div>
+
+	<script src="../43-whiteeng/dist/js/build.js"></script>
+</body>
+
+</html>
